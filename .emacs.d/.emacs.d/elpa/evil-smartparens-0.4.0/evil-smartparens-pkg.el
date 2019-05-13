755f24471835474e4ba31a8b97d24ea4873a1a99
stenssogn <kevin.001@live.se>
2019-05-13 19:10:51 +0200

    ...

---
+++ .emacs.d/.emacs.d/elpa/evil-smartparens-0.4.0/evil-smartparens-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-smartparens" "0.4.0" "Evil support for smartparens" '((evil "1.0") (emacs "24.4") (smartparens "1.10.1")) :commit "9fe4eed1c6327197afe6c13bb0771e18908aff00" :keywords '("evil" "smartparens") :authors '(("Lars Andersen" . "expez@expez.com")) :maintainer '("Lars Andersen" . "expez@expez.com") :url "https://www.github.com/expez/evil-smartparens")
